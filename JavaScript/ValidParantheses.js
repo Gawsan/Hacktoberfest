24db5ecbb80373db84db37ab414921ca2c896c8d
Pratheesh <pratheesh1218@gmail.com>
2022-10-21 15:40:37 +0530

    ValidParantheses

---
+++ JavaScript/ValidParantheses.js
@@ -0,0 +1,22 @@
+const isValid = (s) => {
+    if(s.length % 2 !== 0){
+        return false;
+    }
+
+    const stack = [];
+    const map = new Map([
+        ['(',')'],
+        ['[',']'],
+        ['{','}']
+    ]);
+
+
+    for(let i=0;i<s.length;i+=1){
+        if(map.has(s[i])){
+            stack.push(map.get(s[i]));
+        }else if(s[i] !== stack.pop()){
+            return false;
+        }
+    }
+    return stack.length === 0;
+}
