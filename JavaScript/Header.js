a0e45fcbd41ba92920c79ccaab58e1afbff144e6
Theeban <theebanrajuthayakumar10@gmail.com>
2022-10-29 15:44:24 +0530

    responsive-header

---
+++ JavaScript/Header.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+  return (
+    <nav className="nav">
+
+    </nav>
+  );
+}
+
+export default Navbar;
