442985370a0c7e7345d067929c9e494bb0ac4e59
Theeban <theebanrajuthayakumar10@gmail.com>
2022-10-29 15:44:32 +0530

    responsive-header

--- JavaScript/Header.js
+++ JavaScript/Header.js
@@ -16,7 +16,31 @@ function Navbar() {
   };
   return (
     <nav className="nav">
-
+      <a href="/#" className="nav__brand">
+      FT Marketing
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            Contact
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
     </nav>
   );
 }
