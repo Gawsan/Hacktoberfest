fc9d554a5c5c2f0b6cdfe8be11359ea37e8653db
Theeban <theebanrajuthayakumar10@gmail.com>
2022-10-29 15:39:25 +0530

    footer basic

---
+++ JavaScript/Footer.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="footer">
+            <div className="footer-main-container">
+                <div className="footer-brand">
+                <div className="footer-brand-top">
+                    <div className="footer-brand-logo">
+                    <a href="/#">
+                        <img
+                        src="https://www.logomaker.com/wpstatic/uploads/2021/10/LogoMaker.jpg"
+                        alt=""
+                        className="footer-brand-logo-img"
+                        />
+                    </a>
+                    </div>
+                    <div className="footer-brand-copyrights">
+                    <div className="footer-brand-copyrights-container">
+                        <a href="/#">
+                        <h1>FT Marketing</h1>
+                        </a>
+                        <div className="footer-brand-copyrights-text">
+                        2020 - 2022 FT Marketing | All Rights Reserved
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+
+                <div className="footer-subs-wrapper">
+                <div className="footer-sub-containers">
+                    <div className="footer-sub-containers-header">About Us</div>
+                    <div className="footer-sub-containers-contents">Company</div>
+                    <div className="footer-sub-containers-contents">Blog</div>
+                    <div className="footer-sub-containers-contents">Help</div>
+                </div>
+                <div className="footer-sub-containers">
+                    <div className="footer-sub-containers-header">Legal</div>
+                    <div className="footer-sub-containers-contents">Terms & Condition</div>
+                    <div className="footer-sub-containers-contents">Privacy Policy</div>
+                </div>
+                </div>
+            </div>
+            <div className="footer-bottom-container">
+                <div className="footer-bottom-text">
+                    Ullamco labore proident nulla labore commodo quis quis commodo nostrud. Aliquip cillum enim adipisicing laboris id fugiat fugiat in qui mollit fugiat. Velit voluptate id laborum ut et ex. Excepteur sunt nulla ipsum incididunt sunt nostrud. Anim exercitation adipisicing officia eu sint ullamco dolore fugiat minim occaecat Lorem. Aliqua eiusmod esse qui aliquip commodo. Sint eu aliqua irure esse duis Lorem labore.
+                    Cupidatat ex minim deserunt quis mollit eu excepteur veniam ullamco. Cillum amet occaecat id aute do sint cupidatat officia ex. Fugiat aute consequat labore dolor ad nulla velit minim excepteur. Lorem sint esse consequat velit. Lorem in consectetur do do veniam officia quis fugiat consectetur duis nostrud. In pariatur incididunt anim consequat.
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Footer
