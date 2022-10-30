224419b50a71121a02e08597829d941470b1bae1
vethu <79099028+vethu@users.noreply.github.com>
2022-10-30 14:21:44 +0530

    string java

---
+++ Java/string.java
@@ -0,0 +1,10 @@
+public class StringExample{    
+public static void main(String args[]){    
+String s1="java";//creating string by Java string literal    
+char ch[]={'s','t','r','i','n','g','s'};    
+String s2=new String(ch);//converting char array to string    
+String s3=new String("example");//creating Java string by new keyword    
+System.out.println(s1);    
+System.out.println(s2);    
+System.out.println(s3);    
+}}   
